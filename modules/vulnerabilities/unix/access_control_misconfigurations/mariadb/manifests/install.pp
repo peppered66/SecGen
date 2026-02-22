2dcda6fad2d136b52e4d4733e3419a97ff615ed7
peppered66 <W.Smith6872@student.leedsbeckett.ac.uk>
2026-02-22 16:26:10 +0000

    Completed secgen integration of mariadb module. Restructured puppet module to match secgen requirements eg install.pp, service.pp. Added metadata and scenario file to test module manually.

--- modules/vulnerabilities/unix/access_control_misconfigurations/mariadb/manifests/install.pp
+++ modules/vulnerabilities/unix/access_control_misconfigurations/mariadb/manifests/install.pp
@@ -1,22 +1,19 @@
-class mariadb {
+class mariadb::install{
 
-   #$secgen_parameters = secgen_functions::get_parameters($::base64_inputs_file)
-   #$strings_to_leak=$secgen_parameters['strings_to_leak']
+  $secgen_parameters = secgen_functions::get_parameters($::base64_inputs_file)
+  $strings_to_leak=$secgen_parameters['strings_to_leak']
+  $leaked_filenames = $secgen_parameters['leaked_filenames']
 
   $mariatext1 = "A random string."
-  $mariatext2 = "Needs secgen integration (strings_to_leak)"
+  $mariatext2 = $strings_to_leak[0]
+
+  $mariafilename1 = $leaked_filenames[0]
+  $mariafilename2 = $leaked_filenames[1]
 
   #Installs maria package
    package { 'mariadb-server':
     ensure => installed,
    }
-
- #Creates a service for our maria install
-  service { 'mariadb':
-    ensure  => running,
-    enable  => true,
-    require => Package['mariadb-server'],
-  }
 
  #Replaces original config file for misconfiguration
   file { '/etc/mysql/mariadb.conf.d/50-server.cnf':
@@ -68,26 +65,26 @@ class mariadb {
 
  #Runs maria command to insert the first file into the database
  exec { 'insert_mariatext1_into_maria':
-  command => "/bin/sh -c '/usr/bin/mysql -u root database -e \"INSERT INTO files (name, content) VALUES (\\\"mariatext1\\\", \\\"$(cat /opt/mariadb-files/mariatext1.txt)\\\");\"'",
+  command => "/bin/sh -c '/usr/bin/mysql -u root database -e \"INSERT INTO files (name, content) VALUES (\\\"${mariafilename1}\\\", \\\"$(cat /opt/mariadb-files/mariatext1.txt)\\\");\"'",
   path    => ['/usr/bin', '/bin'],
   require => [
     Exec['create_database_and_table'],
     File['/opt/mariadb-files/mariatext1.txt'],
     Service['mariadb'],
   ],
-  unless  => "/usr/bin/mysql -u root database -N -s -e \"SELECT 1 FROM files WHERE name='mariatext1' LIMIT 1;\" | /bin/grep -q 1",
+  unless  => "/usr/bin/mysql -u root database -N -s -e \"SELECT 1 FROM files WHERE name='${mariafilename1}' LIMIT 1;\" | /bin/grep -q 1",
  }
 
   #Runs maria command to insert the second file into the database
   exec { 'insert_mariatext2_into_maria':
-  command => "/bin/sh -c '/usr/bin/mysql -u root database -e \"INSERT INTO files (name, content) VALUES (\\\"mariatext2\\\", \\\"$(cat /opt/mariadb-files/mariatext2.txt)\\\");\"'",
+  command => "/bin/sh -c '/usr/bin/mysql -u root database -e \"INSERT INTO files (name, content) VALUES (\\\"${mariafilename2}\\\", \\\"$(cat /opt/mariadb-files/mariatext2.txt)\\\");\"'",
   path    => ['/usr/bin', '/bin'],
   require => [
     Exec['create_database_and_table'],
     File['/opt/mariadb-files/mariatext2.txt'],
     Service['mariadb'],
   ],
-  unless  => "/usr/bin/mysql -u root database -N -s -e \"SELECT 1 FROM files WHERE name='mariatext2' LIMIT 1;\" | /bin/grep -q 1",
+  unless  => "/usr/bin/mysql -u root database -N -s -e \"SELECT 1 FROM files WHERE name='${mariafilename2}' LIMIT 1;\" | /bin/grep -q 1",
  }
 
   #Runs maria command to add MYSQL anonymous user to database with elevated privilege
